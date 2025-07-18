159e2d0c2818a55bfd6e4b4953b80b3cc27621a8
Samir Memon <92502942+sammymemon@users.noreply.github.com>
2025-07-18 19:19:23 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1752846562506

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_79db3b34._.js",
+  "static/chunks/src_f1d8ec4e._.js"
+],
+    source: "dynamic"
+});
